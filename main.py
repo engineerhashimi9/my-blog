24cdc376a36969716c9a3f968e57f130be1640dc
engineerhashimi9 <engineerhashimi9@gmail.com>
2026-05-21 11:21:28 +0430

    delete the sqlite url

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ class Base(DeclarativeBase):
 
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
-    "DATABASE_URL", "sqlite:///blog.db")
+    "DATABASE_URL")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
